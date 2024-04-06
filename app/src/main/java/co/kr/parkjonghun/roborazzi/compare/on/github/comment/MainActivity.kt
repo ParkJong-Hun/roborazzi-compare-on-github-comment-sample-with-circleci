e44cd1321fb980ec05aa567db0ba14f599bd7a3a
Park JongHun <thesn515@gmail.com>
2024-04-06 18:08:18 +0900

    chore: for test with a change

--- app/src/main/java/co/kr/parkjonghun/roborazzi/compare/on/github/comment/MainActivity.kt
+++ app/src/main/java/co/kr/parkjonghun/roborazzi/compare/on/github/comment/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Ryochan <3")
+                    Greeting("Joshua <3")
                 }
             }
         }
